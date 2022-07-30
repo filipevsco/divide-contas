556c264c6295cb01a4d04940f465cbffe00af553
Filipe Vasconcelos <filipev@me.com>
2022-07-30 05:47:59 -0300

    Save DespesaController.py.

--- controller/DespesaController.py
+++ controller/DespesaController.py
@@ -1,23 +1,24 @@
 from time import sleep
 
 # from model.despesa import Despesa
-# from model.usuario import Usuario
 
 def cadastrar_despesa():
     print("=== CADASTRAR DESPESA ===")
     print("-------------------------")
-    user = input("Digite o nome de usuario: ")
-    for usuario in Usuario:
-        if usuario.name == user:
-            pagante = usuario
-        else:
-            print("Usuario invalido. tente novamente")
-            sleep(2)
-            cadastrar_despesa()
+    print
+    ##### PENDENTE LISTAR USUÁRIOS 
+  #  for usuario in usuarios:
+  #     print(f'{usuario.id} - {usuario.nome}')
+    print("-------------------------")
+    pagante = int(input("Digite o nome de usuario: "))
+    if pagante == usuario.name:
+        pagante = usuario
     nome = input("Nome Despesa: ")
     categoria = input("Categoria: ")
     valor = float(input("valor: "))
     coop = int(input("compartilhado? (S/N"))
+    
+    despesa = Despesa(pagante, nome, categoria, valor, coop)
 
 def listar_despesas():
     pass
