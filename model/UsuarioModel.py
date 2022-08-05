176fe3b049df21a0989efbe0ff5dbd239ac77ddb
Filipe Vasconcelos <filipev@me.com>
2022-08-05 06:17:27 -0300

    Save UsuarioModel.py.

--- model/UsuarioModel.py
+++ model/UsuarioModel.py
@@ -15,6 +15,14 @@ def conectar():
     except MySQLdb.Error as erro:
         print(f'Erro ao conectar no banco de dados: {erro}')
 
+
+def desconectar():
+    '''
+    deeconectar do banco de dados
+    '''
+    if conectar:
+        conectar.close()
+
 def usuario_db(nome, salario_medio):
      
     
