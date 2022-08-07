f507993d6d12d364b971c397d5bc888d14713b81
Filipe Vasconcelos <filipev@me.com>
2022-08-07 09:19:17 -0300

    inserir e listar usuarios model

--- model/UsuarioModel.py
+++ model/UsuarioModel.py
@@ -1,53 +1,58 @@
 import MySQLdb
 
-def conexao():
+def conectar():
     '''
-    conectar ao banco de dados
+    conectar ao banco de dados / connect to database
     '''
     try:
-        conectar = MySQLdb.connect(
+        conexao = MySQLdb.connect(
             db='divide_contas',
             host='localhost',
             user='admin',
             passwd='admin'
             )
-        return conectar
+        return conexao
     except MySQLdb.Error as erro:
         print(f'Erro ao conectar no banco de dados: {erro}')
 
 
 def desconectar(conexao):
     '''
-    deeconectar do banco de dados
+    desconectar do banco de dados / desconnect to database
     '''
     if conexao:
         conexao.close()
         
 
-def cadaatrar_usuario_db(nome, salario_medio):
-
-    conectar = conexao()
-    cursor = conectar.cursor()
-    cursor.execute(f"INSERT INTO usuarios (nome, salario_medio)) VALUE ('{nome}', {salario_medio})
-    conectar.commit()
+def cadastrar_usuario_db(nome, salario_medio):
+    '''
+    cadastrar novo usuario / add new user
+    '''
+    conexao = conectar()
+    cursor = conexao.cursor()
+    cursor.execute(f"INSERT INTO usuarios (nome, salario_medio)) VALUE ('{nome}', {salario_medio}")
+    conexao.commit()
     
     if cursor.rowcount == 1:
-        print("Usuario cadastrado com sucesso")
+        print(f"Usuario {nome} cadastrado com sucesso")
     else:
         print("erro ao efetuar cadastro")
+    desconectar(conexao)
     
 def listar_usuario_db():
     
-    conectar = conexao()
-    cursor = conectar.cursor()
+    conexao = conectar()
+    cursor = conexao.cursor()
     cursor.execute("SELECT * FROM usuarios")
     usuarios = cursor.ferchall()
     
     if usuarios:
         for usuario in usuarios:
+            print('----LISTANDO USUÁRIOS----')
             print(f'ID: {usuario[0]}')
-            print(f'nome: {usuario[1]')
-            print("----------------")
+            print(f'Nome: {usuario[1]')
+            print(f'Salário: {usuario[2]')
+            print('-------------------------')
     else:
         print("lista vazia")
     
