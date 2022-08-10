f3cbb03c820981e97a9a39c759392ead33d1ff8b
Filipe Vasconcelos <filipev@me.com>
2022-08-10 09:12:43 -0300

    Save despesa.py.

--- db/despesa.py
+++ db/despesa.py
@@ -6,7 +6,12 @@ def listar_despesas():
     lista todas as despesas cadastradas
     '''
     conexao = mysql.conectar()
+    cursor = conexao.cursor()
     
+    cursor.execute(f"SELECT * FROM despesas")
+    despesas = cursor.ferchall()
+    for despesa in despesas:
+        print(f"ID: {despesa[0]}")
 
 
 def cadastrar_despesa():
