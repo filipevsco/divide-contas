3e56b025bfff1128043ad537d9a4be28eecfff44
Filipe Vasconcelos <filipev@me.com>
2022-08-20 05:46:59 -0300

    Save dados_editar.py.

--- view/dados_editar.py
+++ view/dados_editar.py
@@ -4,6 +4,7 @@ def menu_editar_dados():
     print("---MENU DE EDIÇÃO---")
     
     print(" [1] - cadastrar nova categoria")
+    print(" [2] - Excluir categoria")
     print(" [2] - editar usuário")
     print(" [3] - Excluir usuário")
     print(" [4] - Excluir despesa")
